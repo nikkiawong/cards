822ca04216bd56c1823b26c9587c71893f0e88e1
Krystal Foster and Nikki Wong <nikki@>
2018-08-13 14:29:43 -0700

    set up 52 item list and span class

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  
+
+  var ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
+  var suits = ['diamonds', 'spades', 'clubs', 'hearts'];
+
+  ranks.forEach(function(rank) {
+    suits.forEach(function(suit) {
+      $(ranks + suit).text();
+    })
+  })
 })
+
+
+$(rank + " of " + suit).text();
